a80a11e6fe79bdb084690abc01fe42950e39c2f2
Guolong Li <Guolong.LiRealtor@gmail.com>
2022-08-30 13:44:42 -0500

    ChristmasWithTypes

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -5,10 +5,10 @@ namespace ChristmasWithTypes
     {
         public string[] Presents;
         public string Santa { get; set; }
-        public int TreeHeight { get; set; } //TODO Make the Height property nullable
+        public string TreeHeight { get; set; } //TODO Make the Height property nullable
 
         //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        public enum Day {"sunday", "monday", "tuesday", "wednesday", "thursday", "Friday", "Saturday"};
 
     }
 }
